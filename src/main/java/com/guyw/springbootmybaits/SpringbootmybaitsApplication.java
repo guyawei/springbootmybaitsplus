b507c5b99dbc6f6318b6ca3f1c1706f140b0ab3c
guyw <guyawei0819@163.com>
2021-09-04 12:26:13 +0800

    提交初始化

---
+++ src/main/java/com/guyw/springbootmybaits/SpringbootmybaitsApplication.java
@@ -0,0 +1,13 @@
+package com.guyw.springbootmybaits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmybaitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootmybaitsApplication.class, args);
+    }
+
+}
